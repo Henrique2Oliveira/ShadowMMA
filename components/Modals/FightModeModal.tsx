e3a9c55321fe6f3e919a381fd2e2da572bb00d9c
Henrique2Oliveira <henriqueoliveira_2021@outlook.com>
2025-09-01 15:52:07 +0000

    feat(game): update number of rounds in FightModeModal options

--- components/Modals/FightModeModal.tsx
+++ components/Modals/FightModeModal.tsx
@@ -45,10 +45,10 @@ export const FIGHT_OPTIONS: {
   ],
   numberOfRounds: [
     { value: '1', label: '1' },
-    { value: '3', label: '3' },
-    { value: '5', label: '5' },
-    { value: '7', label: '7' },
-    { value: '10', label: '10' },
+  { value: '2', label: '2' },
+  { value: '3', label: '3' },
+  { value: '5', label: '5' },
+  { value: '7', label: '7' },
   ],
   restTimes: [
     { value: '0.5', label: '30s' },
