cf18430b280b02be33b5e8d70cd5545a308fcdcf
Henrique2Oliveira <henriqueoliveira_2021@outlook.com>
2025-10-25 17:24:42 +0000

    feat: remove deprecated PlansModal component to streamline navigation to Plans screen

--- components/Modals/PlansModal.tsx
+++
@@ -1,8 +0,0 @@
-
-type Props = {};
-// Deprecated component placeholder. This file remains only to avoid deletion limitations in this environment.
-// All usages have been removed. Plans flow now routes to '/(protected)/plans'.
-export default function PlansModal() {
-  return null;
-}
-  // TODO: Wire up RevenueCat SDK purchase/restore flows here.
